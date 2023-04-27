0725e374f5f85da552a62d7bf30f644b85ac27d2
hyejin0705 <hyejin3681@gmail.com>
2023-04-27 10:43:50 +0900

    FE:init:setting pages

---
+++ src/pages/club/ClubChatPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ClubChatPage() {
+    return <div>ClubChatPage</div>
+}
+
+export default ClubChatPage
---
+++ src/pages/club/SearchClubPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SearchClubPage() {
+    return <div>SearchClubPage</div>
+}
+
+export default SearchClubPage
