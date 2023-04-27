f83f918044d40a0acafcd39217f42e4ee09b6c11
teamzamong <teamzamong@gmail.com>
2023-04-27 10:44:06 +0900

    BE:feat: add AssetInfo entity

---
+++ backend/src/main/java/org/lightnsalt/hikingdom/domain/info/entity/AssetInfo.java
@@ -0,0 +1,52 @@
+package org.lightnsalt.hikingdom.domain.info.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "asset_info")
+public class AssetInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false, columnDefinition = "BIGINT UNSIGNED")
+	private Long id;
+
+	@Column(nullable = false, length = 50)
+	private String name;
+
+	@Column(name = "asset_url", nullable = false, length = 512)
+	private String assetUrl;
+
+	@Column(nullable = false, length = 512)
+	private String rule;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "mountain_id")
+	@ToString.Exclude
+	private MountainInfo mountainInfo;
+
+	@Builder
+	public AssetInfo(String name, String assetUrl, String rule, MountainInfo mountainInfo) {
+		this.name = name;
+		this.assetUrl = assetUrl;
+		this.rule = rule;
+		this.mountainInfo = mountainInfo;
+	}
+}
