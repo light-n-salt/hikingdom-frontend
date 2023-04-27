16bb75c36105c01735a1a27d3fdd2458d71d5af5
teamzamong <teamzamong@gmail.com>
2023-04-28 01:07:25 +0900

    BE:refactor: remove unnecessary characters from regex

--- backend/src/main/java/org/lightnsalt/hikingdom/domain/member/dto/request/MemberSignUpReq.java
+++ backend/src/main/java/org/lightnsalt/hikingdom/domain/member/dto/request/MemberSignUpReq.java
@@ -12,7 +12,7 @@ public class MemberSignUpReq {
 	private String email;
 
 	@NotEmpty(message = "닉네임은 필수 입력값입니다.")
-	@Pattern(regexp = "[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9]{2,16}", message = "닉네임 형식에 맞지 않습니다.")
+	@Pattern(regexp = "[ㄱ-ㅎ가-힣a-zA-Z0-9]{2,16}", message = "닉네임 형식에 맞지 않습니다.")
 	private String nickname;
 
 	@NotEmpty(message = "비밀번호는 필수 입력값입니다.")
