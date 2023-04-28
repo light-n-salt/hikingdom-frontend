0f003b64471a3817116994d548b8775cc02c27e2
teamzamong <teamzamong@gmail.com>
2023-04-28 09:14:25 +0900

    BE:feat: add Member email validation & confirm APIs

--- backend/src/main/java/org/lightnsalt/hikingdom/domain/member/controller/MemberSignUpController.java
+++ backend/src/main/java/org/lightnsalt/hikingdom/domain/member/controller/MemberSignUpController.java
@@ -5,8 +5,11 @@ import javax.validation.Valid;
 import org.lightnsalt.hikingdom.common.dto.BaseResponseBody;
 import org.lightnsalt.hikingdom.common.dto.ErrorResponseBody;
 import org.lightnsalt.hikingdom.common.error.ErrorCode;
+import org.lightnsalt.hikingdom.domain.member.dto.request.MemberEmailAuthenticationReq;
+import org.lightnsalt.hikingdom.domain.member.dto.request.MemberEmailReq;
 import org.lightnsalt.hikingdom.domain.member.dto.request.MemberNicknameReq;
 import org.lightnsalt.hikingdom.domain.member.dto.request.MemberSignUpReq;
+import org.lightnsalt.hikingdom.domain.member.service.MemberEmailService;
 import org.lightnsalt.hikingdom.domain.member.service.MemberSignUpService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +31,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberSignUpController {
 	private final MemberSignUpService memberSignUpService;
+	private final MemberEmailService memberEmailService;
 
 	@PostMapping("/signup")
 	public ResponseEntity<?> signup(@RequestBody @Valid MemberSignUpReq memberSignUpReq, BindingResult bindingResult) {
@@ -54,5 +58,33 @@ public class MemberSignUpController {
 		memberSignUpService.checkDuplicateNickname(memberNicknameReq.getNickname());
 
 		return new ResponseEntity<>(BaseResponseBody.of("사용할 수 있는 닉네임입니다"), HttpStatus.OK);
+	}
+
+	@GetMapping("/email-valid")
+	public ResponseEntity<?> emailValid(@RequestBody @Valid MemberEmailReq memberEmailReq,
+		BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return new ResponseEntity<>(ErrorResponseBody.of(ErrorCode.INVALID_INPUT_VALUE,
+				bindingResult.getAllErrors().get(0).getDefaultMessage()),
+				HttpStatus.BAD_REQUEST);
+		}
+
+		memberEmailService.sendAuthenticationEmail(memberEmailReq);
+
+		return new ResponseEntity<>(BaseResponseBody.of("인증 이메일을 전송했습니다. 5분 내에 확인해주세요."), HttpStatus.OK);
+	}
+
+	@GetMapping("/email-confirm")
+	public ResponseEntity<?> emailConfirm(@RequestBody @Valid MemberEmailAuthenticationReq memberEmailAuthenticationReq,
+		BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return new ResponseEntity<>(ErrorResponseBody.of(ErrorCode.INVALID_INPUT_VALUE,
+				bindingResult.getAllErrors().get(0).getDefaultMessage()),
+				HttpStatus.BAD_REQUEST);
+		}
+
+		memberEmailService.confirmAuthenticationEmail(memberEmailAuthenticationReq);
+
+		return new ResponseEntity<>(BaseResponseBody.of("이메일 인증에 성공했습니다"), HttpStatus.OK);
 	}
 }
