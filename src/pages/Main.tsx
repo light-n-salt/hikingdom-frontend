7ea122fcf4aab588f98273d66bdfc0a634d97a16
Yeji <yzii9841@gmail.com>
2023-04-28 13:29:33 +0900

    FE:style: fix UserProfile and UserInfo CSS

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,28 +2,29 @@ import React, { useContext } from 'react'
 import { ThemeContext } from 'styles/ThemeProvider'
 // import styles from './Main.module.scss'
 
-// import UserProfile from 'components/user/UserProfile'
+import UserProfile from 'components/user/UserProfile'
+import Modal from 'components/common/Modal'
 
 export default function Main() {
     const { theme, toggleTheme } = useContext(ThemeContext)
-    // const userRecord = {
-    //     totalHikingCount: 10,
-    //     totalMountainCount: 30,
-    //     totalDuration: '30:23',
-    //     totalDistance: 34,
-    //     totalAlt: 34,
-    // }
+    const userRecord = {
+        totalHikingCount: 10,
+        totalMountainCount: 30,
+        totalDuration: '30:23',
+        totalDistance: 34,
+        totalAlt: 34,
+    }
 
     return (
         <div className={`page ${theme} mobile`}>
             <button onClick={toggleTheme}>다크모드</button>
 
-            {/* <UserProfile
+            <UserProfile
                 imgUrl="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80"
                 nickname="이병호리병"
                 email="조혜진진자라@gamil.com"
                 userRecord={userRecord}
-            /> */}
+            />
         </div>
     )
 }
