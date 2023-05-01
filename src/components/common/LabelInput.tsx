31a2ec3772ad7320cf266a69513f5248e113a6cf
Byongho <unlike96@gmail.com>
2023-05-01 15:48:43 +0900

    FE:feat: create agreement page

--- src/components/common/LabelInput.tsx
+++ src/components/common/LabelInput.tsx
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react'
-import { ThemeContext } from 'styles/ThemeProvider'
 import styles from './LabelInput.module.scss'
 
 type LabelInputProps = {
     label: string
     value: string
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+    isPass?: boolean
     isError?: boolean
     placeholder?: string
     type?: string
@@ -15,23 +15,23 @@ function LabelInput({
     label,
     value,
     onChange,
+    isPass = false,
     isError = false,
     placeholder = '',
     type = 'text',
 }: LabelInputProps) {
-    const { theme } = useContext(ThemeContext)
-
     const error = isError ? styles.error : ''
+    const pass = isPass ? styles.pass : ''
 
     return (
-        <div className={`${styles[theme]} ${styles['label-input']}`}>
+        <div className={`${styles['label-input']}`}>
             <label htmlFor="input">{label}</label>
             <input
                 id="input"
                 value={value}
                 onChange={onChange}
                 placeholder={placeholder}
-                className={`${error}`}
+                className={`${error} ${pass}`}
                 type={type}
             />
         </div>
