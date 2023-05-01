749b75657e7aec1521689ac6cfcfc3fd6d9641f4
hyejin0705 <hyejin3681@gmail.com>
2023-05-01 10:28:45 +0900

    FE:init:Link pages in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,42 @@ import './App.scss'
 import React from 'react'
 import { RecoilRoot } from 'recoil'
 import { Routes, Route } from 'react-router-dom'
-import Main from 'pages/Main'
-import LoginPage from 'pages/auth/LoginPage'
 import ThemeProvider from 'styles/ThemeProvider'
+
+import Main from 'pages/Main'
+// Auth
+import LoginPage from 'pages/auth/LoginPage'
+import AgreementPage from 'pages/auth/AgreementPage'
+import FindPwPage from 'pages/auth/FindPwPage'
+import SignupPage from 'pages/auth/SignupPage'
+
+// Main
+// import MainPage from 'pages/main/MainPage'
+import SearchMtPage from 'pages/main/SearchMtPage'
+import MtDetailPage from 'pages/main/MtDetailPage'
+
+// Rank
+import RankPage from 'pages/rank/RankPage'
+
+// Club
+import SearchClubPage from 'pages/club/SearchClubPage'
+import MainDetailPage from 'pages/club/MainDetailPage'
+import ClubCreatePage from 'pages/club/ClubCreatePage'
+import ClubChatPage from 'pages/club/ClubChatPage'
+// Club -
+import ClubPage from 'pages/club/ClubPage'
+import ClubMainPage from 'pages/club/ClubMainPage'
+import ClubMeetupPage from 'pages/club/ClubMeetupPage'
+import ClubMemberPage from 'pages/club/ClubMemberPage'
+import ClubAlbumPage from 'pages/club/ClubAlbumPage'
+// Club - Meetup
+import MeetupCreatePage from 'pages/club/MeetupCreatePage'
+import MeetupDetailPage from 'pages/club/MeetupDetailPage'
+
+// MyPage
+import ProfilePage from 'pages/profile/ProfilePage '
+import ProfileUpdatePage from 'pages/profile/ProfileUpdatePage'
+import AlarmPage from 'pages/profile/AlarmPage'
 
 function App() {
     return (
@@ -13,21 +46,27 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Main />} />
                     <Route path="/login" element={<LoginPage />} />
-                    {/* <Route path="/agreement" element={<AgreementPage />} />
-                    <Route path="/password" element={<FindPwdPage />} />
+                    <Route path="/agreement" element={<AgreementPage />} />
+                    <Route path="/password" element={<FindPwPage />} />
                     <Route path="/signup" element={<SignupPage />} />
 
-                    // 메인
-                    <Route path="/main" element={<MainPage />} />
+                    {/* 메인 */}
+                    {/* <Route path="/" element={<MainPage />} /> */}
                     <Route path="/mountain/search" element={<SearchMtPage />} />
-                    <Route path="/mountain/detail/:id" element={<MtDetailPage />} />
+                    <Route
+                        path="/mountain/detail/:id"
+                        element={<MtDetailPage />}
+                    />
 
-                    // 순위
+                    {/* 순위 */}
                     <Route path="/rank" element={<RankPage />} />
 
-                    // 모임
+                    {/* 모임 */}
                     <Route path="/club/search" element={<SearchClubPage />} />
-                    <Route path="/club/detail/:id" element={<MainDetailPage />} />
+                    <Route
+                        path="/club/detail/:id"
+                        element={<MainDetailPage />}
+                    />
                     <Route path="/club/create" element={<ClubCreatePage />} />
                     <Route path="/club/chat" element={<ClubChatPage />} />
                     <Route path="/club" element={<ClubPage />}>
@@ -45,10 +84,13 @@ function App() {
                         element={<MeetupDetailPage />}
                     />
 
-                    // 마이페이지
+                    {/* 마이페이지 */}
                     <Route path="/profile" element={<ProfilePage />} />
-                    <Route path="/profile/update" element={<ProfileUpdatePage />} />
-                    <Route path="/alarm" element={<AlarmPage />} /> */}
+                    <Route
+                        path="/profile/update"
+                        element={<ProfileUpdatePage />}
+                    />
+                    <Route path="/alarm" element={<AlarmPage />} />
                 </Routes>
             </ThemeProvider>
         </RecoilRoot>
