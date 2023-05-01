b1a5f03f2e7a3bae1844d8f25fae20cc654032cb
hyejin0705 <hyejin3681@gmail.com>
2023-05-02 06:37:16 +0900

    FE:Feat: create MtDeatailPage excluding Img

--- src/components/main/MtTitle.tsx
+++ src/components/main/MtTitle.tsx
@@ -1,19 +1,41 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from 'styles/ThemeProvider'
 import styles from './MtTitle.module.scss'
+import { time } from 'console'
 
 type MtTitleProps = {
     name: string
     maxAlt: number
     timeDuration: number
-    hikingNumber: number
+    assetUrl: string
 }
 
-function MtTitle({ name, maxAlt, timeDuration, hikingNumber }: MtTitleProps) {
+function MtTitle({ name, maxAlt, timeDuration, assetUrl }: MtTitleProps) {
     const { theme, toggleTheme } = useContext(ThemeContext)
     return (
-        <div className={`box ${theme} ${styles['mt-title']}`}>
-            <span>{name}</span>
+        <div className={`content ${theme} ${styles.mttitle}`}>
+            <h1>{name}</h1>
+            <div className={styles.container}>
+                
+                {/* 높이 */}
+                <div className={styles.content}>
+                    <span className={styles.bold}>{maxAlt}m</span>
+                    <span className={styles.text}>높이</span>
+                </div>
+                
+                <span className={styles.border}>|</span>
+                
+                {/* 왕복시간 */}
+                <div className={styles.content}>
+                    <span className={styles.bold}>약 {timeDuration/60}시간</span>
+                    <span className={styles.text}>왕복시간</span>
+                </div>
+                
+                <span className={styles.border}>|</span>
+                
+                {/* asset */}
+                <img src={assetUrl} className={styles.img} />
+            </div>
         </div>
     )
 }
