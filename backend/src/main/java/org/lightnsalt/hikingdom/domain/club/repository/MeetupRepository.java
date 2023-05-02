27258f044706c0f63360caec615f7373cd2e591c
dnzma13 <dnzma13@naver.com>
2023-05-02 14:39:06 +0900

    BE:feat: add meetupMonthlyList api

--- backend/src/main/java/org/lightnsalt/hikingdom/domain/club/repository/MeetupRepository.java
+++ backend/src/main/java/org/lightnsalt/hikingdom/domain/club/repository/MeetupRepository.java
@@ -1,7 +1,18 @@
 package org.lightnsalt.hikingdom.domain.club.repository;
 
+import java.util.List;
+
 import org.lightnsalt.hikingdom.domain.club.entity.meetup.Meetup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MeetupRepository extends JpaRepository<Meetup, Long> {
+	@Query(value = "SELECT m "
+		+ "FROM Meetup m "
+		+ "WHERE m.club.id = :clubId "
+		+ "AND YEAR(m.startAt) = :year "
+		+ "AND MONTH(m.startAt) = :month ")
+	List<Meetup> findByClubIdAndStartAt(@Param("clubId") Long clubId, @Param("year") int year,
+		@Param("month") int month);
 }
