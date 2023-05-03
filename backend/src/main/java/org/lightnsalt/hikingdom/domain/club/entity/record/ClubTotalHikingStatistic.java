c8bbd045fbb7d2d99cd3b69202d472346f05ccf9
teamzamong <teamzamong@gmail.com>
2023-05-03 12:45:12 +0900

    BE:fix: fix relationship between Club and HikingStatistics

--- backend/src/main/java/org/lightnsalt/hikingdom/domain/club/entity/record/ClubTotalHikingStatistic.java
+++ backend/src/main/java/org/lightnsalt/hikingdom/domain/club/entity/record/ClubTotalHikingStatistic.java
@@ -5,11 +5,10 @@ import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.lightnsalt.hikingdom.domain.club.entity.Club;
@@ -29,33 +28,32 @@ import lombok.ToString;
 @Table(name = "club_total_hiking_statistic")
 public class ClubTotalHikingStatistic {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(columnDefinition = "BIGINT UNSIGNED")
 	private Long id;
 
 	@ToString.Exclude
 	@JsonIgnore
-	@ManyToOne(fetch = FetchType.LAZY)
+	@MapsId
+	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "club_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
 	private Club club;
 
-	@Column(name = "total_hiking_count", nullable = false, columnDefinition = "INT UNSIGNED")
+	@Column(name = "total_hiking_count", nullable = false, columnDefinition = "INT UNSIGNED DEFAULT 0")
 	private Long totalHikingCount;
 
-	@Column(name = "total_mountain_count", nullable = false, columnDefinition = "INT UNSIGNED")
+	@Column(name = "total_mountain_count", nullable = false, columnDefinition = "INT UNSIGNED DEFAULT 0")
 	private Long totalMountainCount;
 
-	@Column(name = "total_duration", nullable = false, columnDefinition = "INT UNSIGNED")
+	@Column(name = "total_duration", nullable = false, columnDefinition = "INT UNSIGNED DEFAULT 0")
 	private Long totalDuration; // in seconds
 
-	@Column(name = "total_distance", nullable = false, columnDefinition = "INT UNSIGNED")
+	@Column(name = "total_distance", nullable = false, columnDefinition = "INT UNSIGNED DEFAULT 0")
 	private Long totalDistance; // in metres
 
-	@Column(name = "total_alt", nullable = false, columnDefinition = "INT UNSIGNED")
+	@Column(name = "total_alt", nullable = false, columnDefinition = "INT UNSIGNED DEFAULT 0")
 	private Long totalAlt; // in metres
 
-	@Column(name = "participation_rate", nullable = false)
-	private double participationRate;
+	@Column(name = "participation_rate", nullable = false, columnDefinition = "DOUBLE DEFAULT 0")
+	private double participationRate; // in %
 
 	@Builder
 	public ClubTotalHikingStatistic(Club club, Long totalHikingCount, Long totalMountainCount, Long totalDuration,
