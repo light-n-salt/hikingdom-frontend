d8bef83c8e01f422198219e315c7d7cccb65ec4e
hyejin0705 <hyejin3681@gmail.com>
2023-05-06 02:50:34 +0900

    FE:feat:Create MemberModal in MeetupMembers Component

--- frontend/src/components/meetup/MeetupMembers.tsx
+++ frontend/src/components/meetup/MeetupMembers.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './MeetupMembers.module.scss'
 
 import Image from 'components/common/Image'
 import Button from 'components/common/Button'
+import Modal from 'components/common/Modal'
+import MemberModal from './MemberModal'
 import { MeetupMember } from 'types/meetup.interface'
 
 type MeetupMembersProps = {
@@ -10,24 +12,33 @@ type MeetupMembersProps = {
 }
 
 function MeetupMembers({ memberInfo }: MeetupMembersProps) {
+  const [isOpen, setIsOpen] = useState(false)
+  
   return (
-    <div className={styles.meetup}>
-      <div className={styles.titles}>
-        <span className={styles.title}>참여 멤버 (N명)</span>
-        <Button text="참여" color="primary" size="xs" />
+    <>
+      {isOpen && (
+        <Modal onClick={() => setIsOpen(false)}>
+          <MemberModal/>
+        </Modal>
+      )}
+      <div className={styles.meetup}>
+        <div className={styles.titles}>
+          <span className={styles.title}>참여 멤버 (N명)</span>
+          <Button text="참여" color="primary" size="xs" />
+        </div>
+        <div className={styles.members}>
+          {memberInfo.map((member) => (
+            <Image
+              key={member.memberId}
+              imgUrl={member.profileUrl}
+              size="sm"
+              isSquare={true}
+            />
+          ))}
+          <div className={styles.more} onClick={() => setIsOpen(true)}>+</div>
+        </div>
       </div>
-      <div className={styles.members}>
-        {memberInfo.map((member) => (
-          <Image
-            key={member.memberId}
-            imgUrl={member.profileUrl}
-            size="sm"
-            isSquare={true}
-          />
-        ))}
-        <div className={styles.more}>+</div>
-      </div>
-    </div>
+    </>
   )
 }
 
