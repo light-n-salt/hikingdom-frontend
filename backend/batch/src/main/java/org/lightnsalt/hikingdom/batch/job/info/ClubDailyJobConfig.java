835fa9582aa801106b79efe165760d7f1d9ddf75
이현진 <dnzma13@naver.com>
2023-05-05 19:39:02 +0900

    BE:init: add clubDailyInfo entity

---
+++ backend/batch/src/main/java/org/lightnsalt/hikingdom/batch/job/info/ClubDailyJobConfig.java
@@ -0,0 +1,12 @@
+package org.lightnsalt.hikingdom.batch.job.info;
+
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class ClubDailyJobConfig {
+}
