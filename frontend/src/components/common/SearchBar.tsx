05f1e91f38d62d31d2808bd1003ad30f8a7c9f4a
Byongho96 <unlike96@gmail.com>
2023-05-06 15:04:10 +0900

    FE:feat: navigte between main and mtsearchpage

--- frontend/src/components/common/SearchBar.tsx
+++ frontend/src/components/common/SearchBar.tsx
@@ -1,32 +1,29 @@
-import React, { useContext } from 'react'
-import { ThemeContext } from 'styles/ThemeProvider'
+import React from 'react'
 import styles from './SearchBar.module.scss'
-import { BsSearch } from 'react-icons/bs'
+import { BiSearch } from 'react-icons/bi'
 
 type SearchBarProps = {
   value: string
   placeholder: string
-  setSelected?: (value: string) => void // selectbox 값 변경 함수
-  onChangeText: (event: React.ChangeEvent<HTMLInputElement>) => void // 검색값
+  // setSelected?: (value: string) => void // selectbox 값 변경 함수
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void // 검색값
 }
 
 function SearchBar({
   value,
   placeholder,
-  setSelected,
-  onChangeText,
+  // setSelected,
+  onChange,
 }: SearchBarProps) {
-  const { theme } = useContext(ThemeContext)
-
   return (
-    <div className={`content ${theme} ${styles.searchbar}`}>
-      {setSelected ? <>toggle</> : <BsSearch className={`${styles.icon}`} />}
-      <label htmlFor="input"> I </label>
+    <div className={`${styles.searchbar}`}>
+      <BiSearch className={`${styles.icon}`} />
       <input
         id="input"
         value={value}
-        onChange={onChangeText}
+        onChange={onChange}
         placeholder={placeholder}
+        className={`${styles.input}`}
       />
     </div>
   )
