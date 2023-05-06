eb913b7cc142ca3dcf9e7d7b90f16264cadfd938
teamzamong <teamzamong@gmail.com>
2023-05-06 20:38:16 +0900

    BE:feat: add CustomLogFilter to log client requests

---
+++ backend/gateway/src/main/java/org/lightnsalt/hikingdom/gateway/filter/CustomLogFilter.java
@@ -0,0 +1,47 @@
+package org.lightnsalt.hikingdom.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class CustomLogFilter implements GatewayFilterFactory<CustomLogFilter.Config> {
+	@Override
+	public GatewayFilter apply(Config config) {
+		return (exchange, chain) -> {
+			ServerHttpRequest request = exchange.getRequest();
+			ServerHttpResponse response = exchange.getResponse();
+
+			if (config.isPreLogger()) {
+				log.info("CustomLogFilter pre-filter: Request Path -> {}", request.getPath());
+			}
+
+			return chain.filter(exchange).then(Mono.fromRunnable(
+				() -> {
+					if (config.isPostLogger()) {
+						log.info("CustomLogFilter post-filter: Response Status -> {}", response.getStatusCode());
+					}
+				}));
+		};
+	}
+
+	@Override
+	public Class<CustomLogFilter.Config> getConfigClass() {
+		return CustomLogFilter.Config.class;
+	}
+
+	@Data
+	public static class Config {
+		private boolean preLogger;
+		private boolean postLogger;
+	}
+}
