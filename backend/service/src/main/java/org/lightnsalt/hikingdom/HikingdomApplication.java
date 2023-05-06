0a3cde9b8899b29a0646f473c2cb643d13b7083d
teamzamong <teamzamong@gmail.com>
2023-05-07 01:17:07 +0900

    BE:fix: update Gateway & Service for service discovery

--- backend/service/src/main/java/org/lightnsalt/hikingdom/HikingdomApplication.java
+++ backend/service/src/main/java/org/lightnsalt/hikingdom/HikingdomApplication.java
@@ -2,9 +2,11 @@ package org.lightnsalt.hikingdom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HikingdomApplication {
 
