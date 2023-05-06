c4145bacc6abc5f95352514e29e1fd69f4862e06
Yeji Jung <yzii9841@gmail.com>
2023-05-06 18:25:10 +0900

    FE:feat: create PhotoModal component

--- frontend/src/components/club/AlbumList.tsx
+++ frontend/src/components/club/AlbumList.tsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './AlbumList.module.scss'
 import { Album } from 'types/club.interface'
+
+import Modal from 'components/common/Modal'
+import PhotoModal from 'components/common/PhotoModal'
 
 type AlbumListProps = {
   photoList: Album[]
 }
 
 function AlbumList({ photoList }: AlbumListProps) {
+  const [isOpen, setIsOpen] = useState(false) // 모달 on/off
+  const [photo, setPhoto] = useState<Album | undefined>(undefined) // 선택한 사진
+
+  // 선택한 사진 모달에 띄우는 함수
+  const onClickOpenModal = (photoId: number) => {
+    const selectedPhoto = photoList.find((photo) => photo.photoId === photoId)
+    if (selectedPhoto) {
+      setPhoto(selectedPhoto)
+      setIsOpen(true)
+    }
+  }
+
   return (
     <div className={styles.container}>
+      {isOpen && (
+        <Modal onClick={() => setIsOpen(false)}>
+          {photo && <PhotoModal photo={photo} />}
+        </Modal>
+      )}
       {photoList.map((photo) => (
-        <img key={photo.photoId} src={photo.imgUrl} className={styles.img} />
+        <img
+          key={photo.photoId}
+          src={photo.imgUrl}
+          className={styles.img}
+          onClick={() => onClickOpenModal(photo.photoId)}
+        />
       ))}
     </div>
   )
