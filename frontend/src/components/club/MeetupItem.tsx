f49bc9c53f24eee305f6b0d101ec3be17095fbcf
Byongho96 <unlike96@gmail.com>
2023-05-08 00:30:53 +0900

    FE:feat: add club meetup page

--- frontend/src/components/club/MeetupItem.tsx
+++ frontend/src/components/club/MeetupItem.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import styles from './MeetupItem.module.scss'
+import clock from 'assets/images/clock.png'
+import person from 'assets/images/person.png'
+import mountain from 'assets/images/mountain.png'
+import calendar from 'assets/images/calendar.png'
+import IconText from 'components/common/IconText'
+import { ThemeContext } from 'styles/ThemeProvider'
+import { MeetupInfo } from 'types/meetup.interface'
 
-function MeetupItem() {
-  return <div>MeetupItem</div>
+type MeetupItemProps = {
+  meetupInfo: MeetupInfo
+}
+
+function MeetupItem({ meetupInfo }: MeetupItemProps) {
+  const { theme } = useContext(ThemeContext)
+  const [date, time] = meetupInfo.startAt.split(' ')
+
+  return (
+    <div className={`content ${theme} ${styles.container}`}>
+      <div className={styles.header}>
+        <h3>{meetupInfo.meetupName}</h3>
+        <IconText
+          imgSrc={person}
+          text={meetupInfo.totalMmember.toString()}
+          size="sm"
+          isBold={true}
+        />
+      </div>
+      <div className={styles.flexbox}>
+        <IconText
+          imgSrc={mountain}
+          text={meetupInfo.mountainName}
+          size="sm"
+          isBold={true}
+        />
+        <IconText imgSrc={calendar} text={date} size="sm" isBold={true} />
+        <IconText imgSrc={clock} text={time} size="sm" isBold={true} />
+      </div>
+      {meetupInfo.description.length > 100
+        ? meetupInfo.description.slice(100) + '...'
+        : meetupInfo.description}
+    </div>
+  )
 }
 
 export default MeetupItem
--- frontend/src/components/club/MeetupList.tsx
+++ frontend/src/components/club/MeetupList.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import styles from './MeetupList.module.scss'
+import MeetupItem from 'components/club/MeetupItem'
+import { MeetupInfo } from 'types/meetup.interface'
 
-function MeetupList() {
-  return <div>MeetupList</div>
+type MeetupListProps = {
+  meetupInfoArray: MeetupInfo[]
+}
+
+function MeetupList({ meetupInfoArray }: MeetupListProps) {
+  return (
+    <div className={styles.container}>
+      {meetupInfoArray.map((meetupInfo) => (
+        <MeetupItem key={meetupInfo.meetupId} meetupInfo={meetupInfo} />
+      ))}
+    </div>
+  )
 }
 
 export default MeetupList
