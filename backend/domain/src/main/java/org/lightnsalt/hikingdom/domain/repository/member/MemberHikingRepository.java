70a3d9f0092e0e22b8a8b46a2741b7ee3a44b1f7
이현진 <dnzma13@naver.com>
2023-05-07 14:44:37 +0900

    BE:feat: add hikingRecordDetail api

--- backend/domain/src/main/java/org/lightnsalt/hikingdom/domain/repository/member/MemberHikingRepository.java
+++ backend/domain/src/main/java/org/lightnsalt/hikingdom/domain/repository/member/MemberHikingRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberHikingRepository extends JpaRepository<MemberHiking, Long> {
 	List<MemberHiking> findTop3ByMemberIdOrderByEndAt(Long id);
+
 }
