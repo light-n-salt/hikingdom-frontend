74a71f2346319bd55cc5feb07aa74739b45786d9
kjyk0835 <kjyk0835@naver.com>
2023-05-08 18:06:34 +0900

    BE:feat: add saveHikingRecord in HikingService

--- backend/service/src/main/java/org/lightnsalt/hikingdom/service/hiking/service/HikingService.java
+++ backend/service/src/main/java/org/lightnsalt/hikingdom/service/hiking/service/HikingService.java
@@ -1,9 +1,12 @@
 package org.lightnsalt.hikingdom.service.hiking.service;
 
+import org.lightnsalt.hikingdom.service.hiking.dto.request.HikingRecordReq;
 import org.lightnsalt.hikingdom.service.hiking.dto.response.TodayMeetupRes;
 
 import java.util.List;
 
 public interface HikingService {
     List<TodayMeetupRes> findTodayMeetup(String email);
+
+    Long saveHikingRecord(String email, HikingRecordReq hikingRecordReq);
 }
