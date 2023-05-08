9437a70de3c59d8134b4b5e5c775ddf558cc62eb
teamzamong <teamzamong@gmail.com>
2023-05-08 11:32:39 +0900

    BE:fix: remove unnecessary annotation

--- backend/service/src/main/java/org/lightnsalt/hikingdom/service/club/service/ClubPhotoService.java
+++ backend/service/src/main/java/org/lightnsalt/hikingdom/service/club/service/ClubPhotoService.java
@@ -3,10 +3,7 @@ package org.lightnsalt.hikingdom.service.club.service;
 import org.lightnsalt.hikingdom.common.dto.CustomSlice;
 import org.lightnsalt.hikingdom.service.club.dto.response.MeetupAlbumRes;
 import org.springframework.data.domain.Pageable;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface ClubPhotoService {
-
-	@Transactional
 	CustomSlice<MeetupAlbumRes> findClubPhotoList(String email, Long clubId, Long photoId, Pageable pageable);
 }
