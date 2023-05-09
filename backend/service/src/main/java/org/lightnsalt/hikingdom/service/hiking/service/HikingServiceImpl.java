5aa990b42cea8480c1da47dfa8e52562eea6f91d
kjyk0835 <kjyk0835@naver.com>
2023-05-09 16:10:51 +0900

    BE:feat: add HikingServiceImpl to implement Tracking Info Save APi

--- backend/service/src/main/java/org/lightnsalt/hikingdom/service/hiking/service/HikingServiceImpl.java
+++ backend/service/src/main/java/org/lightnsalt/hikingdom/service/hiking/service/HikingServiceImpl.java
@@ -1,22 +1,39 @@
 package org.lightnsalt.hikingdom.service.hiking.service;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.lightnsalt.hikingdom.common.error.ErrorCode;
 import org.lightnsalt.hikingdom.common.error.GlobalException;
 import org.lightnsalt.hikingdom.domain.entity.club.meetup.Meetup;
+import org.lightnsalt.hikingdom.domain.entity.hiking.MemberHiking;
+import org.lightnsalt.hikingdom.domain.entity.hiking.MemberHikingGps;
+import org.lightnsalt.hikingdom.domain.entity.info.MountainInfo;
+import org.lightnsalt.hikingdom.domain.entity.member.Member;
 import org.lightnsalt.hikingdom.service.club.dto.response.MeetupDailyRes;
 import org.lightnsalt.hikingdom.service.club.repository.ClubRepository;
 import org.lightnsalt.hikingdom.service.club.repository.meetup.MeetupMemberRepository;
 import org.lightnsalt.hikingdom.service.club.repository.meetup.MeetupRepository;
+import org.lightnsalt.hikingdom.service.hiking.dto.request.GpsRoute;
 import org.lightnsalt.hikingdom.service.hiking.dto.request.HikingRecordReq;
 import org.lightnsalt.hikingdom.service.hiking.dto.response.TodayMeetupRes;
 import org.lightnsalt.hikingdom.service.hiking.repository.HikingRepositoryCustom;
+import org.lightnsalt.hikingdom.service.hiking.repository.MemberHikingGpsRepository;
+import org.lightnsalt.hikingdom.service.hiking.repository.MemberHikingRepository;
 import org.lightnsalt.hikingdom.service.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -28,6 +45,8 @@ public class HikingServiceImpl implements HikingService {
     private final MeetupRepository meetupRepository;
     private final MeetupMemberRepository meetupMemberRepository;
     private final MemberRepository memberRepository;
+    private final MemberHikingRepository memberHikingRepository;
+    private final MemberHikingGpsRepository memberHikingGpsRepository;
     private final HikingRepositoryCustom hikingRepositoryCustom;
 
 //    @Override
@@ -79,11 +98,73 @@ public class HikingServiceImpl implements HikingService {
     @Override
     @Transactional
     public Long saveHikingRecord(String email, HikingRecordReq hikingRecordReq) {
-        // 회원 확인
-        final Long memberId = memberRepository.findByEmailAndIsWithdraw(email, false)
-                .orElseThrow(() -> new GlobalException(ErrorCode.MEMBER_NOT_FOUND))
-                .getId();
+        final Member member = memberRepository.findByEmailAndIsWithdraw(email, false)
+                .orElseThrow(() -> new GlobalException(ErrorCode.MEMBER_UNAUTHORIZED));
 
-        return null;
+        LocalDateTime startAt = LocalDateTime.parse(hikingRecordReq.getStartAt(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        LocalDateTime endAt = startAt.plusMinutes(hikingRecordReq.getTotalDuration());
+
+        Meetup meetup = meetupRepository.findById(hikingRecordReq.getMeetupId())
+                .orElseThrow(() -> new GlobalException(ErrorCode.MEETUP_NOT_FOUND));
+
+        MountainInfo mountainInfo = meetup.getMountain();
+
+        MemberHiking memberHiking = MemberHiking.builder()
+                .member(member)
+                .mountain(mountainInfo)
+                .meetup(meetup)
+                .isMeetup(hikingRecordReq.getIsMeetup())
+                .startAt(startAt)
+                .endAt(endAt)
+                .totalDuration((long) hikingRecordReq.getTotalDuration())
+                .totalDistance((long) hikingRecordReq.getTotalDistance())
+                .totalAlt((long) hikingRecordReq.getMaxAlt())
+                .isSummit(true) // 완등여부 => 임시로 true
+                .build();
+
+
+//        hikingRecordReq.getGpsRoute()
+        JsonObject gpsRoute = new JsonObject();
+
+//        JsonArray jsonArray = new JsonArray();
+//        jsonArray.add(hikingRecordReq.getGpsRoute());
+        JsonArray jsonArray = new JsonArray();
+        for (GpsRoute gps : hikingRecordReq.getGpsRoute()) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("lat", gps.getLat());
+            jsonObject.addProperty("lng", gps.getLng());
+            jsonObject.addProperty("alt", gps.getAlt());
+            jsonArray.add(jsonObject);
+        }
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("gpsRoute", jsonArray);
+        gpsRoute.add("gpsRoute", jsonArray);
+        MemberHikingGps memberHikingGps = MemberHikingGps.builder()
+                .hiking(memberHiking)
+                .gpsRoute(getMapFromJsonObject(gpsRoute))
+                .build();
+
+        final MemberHiking savedMemberHiking = memberHikingRepository.save(memberHiking);
+        final MemberHikingGps savedMemberHikingGps = memberHikingGpsRepository.save(memberHikingGps);
+
+        return savedMemberHiking.getId();
+    }
+
+    public static Map<String, Object> getMapFromJsonObject(JsonObject jsonObj){
+        Map<String, Object> map = null;
+
+        try {
+            map = new ObjectMapper().readValue(jsonObj.toString(), Map.class);
+        } catch (JsonParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return map;
     }
 }
