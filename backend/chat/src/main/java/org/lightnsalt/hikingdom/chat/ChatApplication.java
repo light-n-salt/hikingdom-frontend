d02ecbe01867f3347f582e59402e7c39bad0bd67
teamzamong <teamzamong@gmail.com>
2023-05-10 17:28:03 +0900

    BE:fix: set MongoRepository, JpaRepository & entities

--- backend/chat/src/main/java/org/lightnsalt/hikingdom/chat/ChatApplication.java
+++ backend/chat/src/main/java/org/lightnsalt/hikingdom/chat/ChatApplication.java
@@ -2,6 +2,7 @@ package org.lightnsalt.hikingdom.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -14,9 +15,10 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @EnableEurekaClient
 @EnableMongoAuditing
-@EnableMongoRepositories(basePackages = "org.lightnsalt.hikingdom.chat.repository.mongo")
+@EnableMongoRepositories("org.lightnsalt.hikingdom.chat.repository.mongo")
 @EnableJpaAuditing
-@EnableJpaRepositories(basePackages = "org.lightnsalt.hikingdom.chat.repository.mysql")
+@EnableJpaRepositories("org.lightnsalt.hikingdom.chat.repository.mysql")
+@EntityScan({"org.lightnsalt.hikingdom.domain.entity", "org.lightnsalt.hikingdom.chat.entity"})
 public class ChatApplication {
 
 	public static void main(String[] args) {
