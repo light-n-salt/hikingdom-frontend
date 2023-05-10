29d36518c568333809e9ded159cac9dae701ea49
Byongho <unlike96@gmail.com>
2023-05-10 11:09:26 +0900

    FE:feat: add policy page and delete applying club buuton

---
+++ frontend/src/pages/auth/PolicyPage.tsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react'
+import styles from './PolicyPage.module.scss'
+import { PERSONAL_AGREEMENT, USAGE_AGREEMENT } from 'constants/agreements'
+import PageHeader from 'components/common/PageHeader'
+import LabelTextArea from 'components/common/LabelTextArea'
+import { ThemeContext } from 'styles/ThemeProvider'
+
+function PolicyPage() {
+  const { theme } = useContext(ThemeContext)
+  return (
+    <div className={`page ${theme} p-lg ${styles.container}`}>
+      <div className={styles.container}>
+        <PageHeader title="약관 정책" />
+        <div className={styles.form}>
+          <LabelTextArea
+            label="이용약관"
+            value={USAGE_AGREEMENT}
+            size="lg"
+            placeholder=""
+            disabled={true}
+          />
+        </div>
+        <div className={styles.form}>
+          <LabelTextArea
+            label="개인정보 수집 및 이동"
+            value={PERSONAL_AGREEMENT}
+            size="lg"
+            placeholder=""
+            disabled={true}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PolicyPage
