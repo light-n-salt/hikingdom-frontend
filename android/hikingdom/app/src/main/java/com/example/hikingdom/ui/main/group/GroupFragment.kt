159491a8837e2a937d30f1f3cf09211ad287d9c4
kjyk0835 <kjyk0835@naver.com>
2023-05-10 13:28:17 +0900

    AND:feat: add webview settings: MIXED_CONTENT_ALWAYS_ALLOW and modify webView.settings.domStorageEnabled true, modify base url to GroupFragment, MypageFragment, RankingFragment

--- android/hikingdom/app/src/main/java/com/example/hikingdom/ui/main/group/GroupFragment.kt
+++ android/hikingdom/app/src/main/java/com/example/hikingdom/ui/main/group/GroupFragment.kt
@@ -1,5 +1,7 @@
 package com.example.hikingdom.ui.main.group
 
+import android.os.Build
+import android.webkit.WebSettings
 import android.webkit.WebViewClient
 import androidx.activity.OnBackPressedCallback
 import com.example.hikingdom.ApplicationClass.Companion.GROUP_WEB_URL
@@ -21,6 +23,23 @@ class GroupFragment(): BaseFragment<FragmentGroupBinding>(FragmentGroupBinding::
         webView.webViewClient = WebViewClient()
         webView.loadUrl(GROUP_WEB_URL)
         webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        val wsetting: WebSettings = webView.settings
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { // https 이미지.
+            wsetting.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        }
+
+        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    System.exit(0)
+                }
+            }
+        })
 
         activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
--- android/hikingdom/app/src/main/java/com/example/hikingdom/ui/main/ranking/RankingFragment.kt
+++ android/hikingdom/app/src/main/java/com/example/hikingdom/ui/main/ranking/RankingFragment.kt
@@ -1,5 +1,7 @@
 package com.example.hikingdom.ui.main.ranking
 
+import android.os.Build
+import android.webkit.WebSettings
 import android.webkit.WebViewClient
 import androidx.activity.OnBackPressedCallback
 import com.example.hikingdom.ApplicationClass.Companion.RANKING_WEB_URL
@@ -21,6 +23,23 @@ class RankingFragment(): BaseFragment<FragmentRankingBinding>(FragmentRankingBin
         webView.webViewClient = WebViewClient()
         webView.loadUrl(RANKING_WEB_URL)
         webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        val wsetting: WebSettings = webView.settings
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { // https 이미지.
+            wsetting.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        }
+
+        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    System.exit(0)
+                }
+            }
+        })
 
         activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
