bad9c02ae6a6a56cca6b77bc5fa02fcf3478dd91
teamzamong <teamzamong@gmail.com>
2023-05-11 11:30:16 +0900

    BE:fix: remove withSockJS

--- backend/chat/src/main/java/org/lightnsalt/hikingdom/chat/config/WebSocketConfig.java
+++ backend/chat/src/main/java/org/lightnsalt/hikingdom/chat/config/WebSocketConfig.java
@@ -19,8 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/chat")
 			.setAllowedOriginPatterns("http://localhost:3000", "http://localhost:8080", "https://hikingdom.kr",
-				"http://hikingdom.kr:3001", "https://k8a102.p.ssafy.io")
-			.withSockJS();
+				"http://hikingdom.kr:3001", "https://k8a102.p.ssafy.io");
 	}
 }
 
