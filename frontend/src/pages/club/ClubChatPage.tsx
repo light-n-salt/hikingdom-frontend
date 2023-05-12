de23d0b1280660da1a2fb3596fe3089d646f8218
Yeji Jung <yzii9841@gmail.com>
2023-05-12 09:17:15 +0900

    FE:fix: Chat infiniteScroll

--- frontend/src/pages/club/ClubChatPage.tsx
+++ frontend/src/pages/club/ClubChatPage.tsx
@@ -1,20 +1,28 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext, useState, useEffect, useRef } from 'react'
 import { ThemeContext } from 'styles/ThemeProvider'
-import { useParams } from 'react-router-dom'
 import PageHeader from 'components/common/PageHeader'
 import ChatList from 'components/club/ChatList'
 import Loading from 'components/common/Loading'
 import { Chats, Chat, ChatMember } from 'types/chat.interface'
 import { getChats, getMembers, getClubSimpleInfo } from 'apis/services/clubs'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useInfiniteQuery } from '@tanstack/react-query'
 import useUserQuery from 'hooks/useUserQuery'
 import sockjs from 'sockjs-client'
 import { Stomp } from '@stomp/stompjs'
-import axios from 'axios'
 import TextSendBar from 'components/common/TextSendBar'
+import useScroll from 'hooks/useScroll'
+
+type InfiniteChat = {
+  content: Chats[]
+  hasNext: boolean
+  hasPrevious: boolean
+  numberOfElements: number
+  pageSize: number
+}
 
 function ClubChatPage() {
   const { theme } = useContext(ThemeContext)
+  const infiniteRef = useRef<HTMLDivElement>(null)
   const { data: userInfo } = useUserQuery() // 유저 정보
 
   // 모임정보
@@ -30,23 +38,45 @@ function ClubChatPage() {
   const [stomp, setStomp] = useState<any>() // 타입 수정 필요
 
   // 채팅 & 멤버 데이터
-  // useState<{ [key: number]: ChatMember }
-  const [members, setMembers] = useState<any>({})
+  const [members, setMembers] = useState<{ [key: number]: ChatMember }>({})
   const [chatList, setChatList] = useState<Chat[]>([])
   const [message, setMessage] = useState<string>('')
 
-  // 초기 데이터
+  // 채팅 데이터
   const { isLoading, isError } = useQuery<any>(
     ['chats'],
     () => getChats(Number(userInfo?.clubId)),
     {
       onSuccess: (res) => {
+        console.log(res.chats.content.slice(-1)[0].chatId)
         setChatList(res.chats.content)
       },
       enabled: !!userInfo,
     }
   )
+  // const {
+  //   data: chats,
+  //   isError,
+  //   isLoading,
+  //   fetchNextPage,
+  //   hasNextPage,
+  // } = useInfiniteQuery<any>({
+  //   queryKey: ['chats'],
+  //   queryFn: ({ pageParam = null }) => {
+  //     return () => getChats(Number(userInfo?.clubId), pageParam, 20)
+  //   },
+  //   getNextPageParam: (lastPage) => {
+  //     console.log('lastPage', lastPage)
+  //     return lastPage.isLast
+  //       ? lastPage.chats.content.slice(-1)[0].chatId
+  //       : undefined
+  //   },
+  //   onSuccess: (res) => {
+  //     console.log('data', res)
+  //   },
+  // })
 
+  // 멤버 데이터
   const { data: memberInfo } = useQuery(
     ['members'],
     () => getMembers(Number(userInfo?.clubId)),
@@ -70,8 +100,7 @@ function ClubChatPage() {
   }, [])
 
   const connection = () => {
-    const socket = new sockjs('http://hikingdom.kr:8081/chat')
-    console.log('socket', socket)
+    const socket = new sockjs('https://hikingdom.kr/chat')
     const stomp = Stomp.over(socket)
     setStomp(stomp)
 
@@ -81,7 +110,7 @@ function ClubChatPage() {
       stomp.subscribe(`/sub/clubs/${userInfo?.clubId}`, (chatDTO) => {
         // 구독후 메세지를 받을 때마다 실행할 함수
         const data = JSON.parse(chatDTO.body)
-
+        console.log('data', data)
         // 채팅 데이터
         if (data.type === 'MESSAGE') {
           setChatList((chatList) => [data.chat, ...chatList])
@@ -103,7 +132,7 @@ function ClubChatPage() {
       `/pub/clubs/${userInfo?.clubId}`,
       {},
       JSON.stringify({
-        status: 'chat',
+        type: 'chat',
         clubId: userInfo?.clubId,
         memberId: userInfo?.memberId,
         content: message,
@@ -114,10 +143,7 @@ function ClubChatPage() {
 
   return (
     <div className={`page p-sm ${theme} mobile `}>
-      <PageHeader
-        title={clubInfo?.clubName}
-        url={`/club/${userInfo?.clubId}/main`}
-      />
+      <PageHeader title={clubInfo?.clubName} url={`/club/main`} />
       {isError || isLoading ? (
         <Loading />
       ) : (
