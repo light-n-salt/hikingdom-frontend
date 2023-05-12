f442410410733a6321462c1e54ec42421996b0e1
Yeji <yzii9841@gmail.com>
2023-05-12 17:01:21 +0900

    FE:feat: allow users to see one anothers profile page

--- frontend/src/components/user/UserProfile.tsx
+++ frontend/src/components/user/UserProfile.tsx
@@ -10,18 +10,19 @@ import UserInfo from 'components/user/UserInfo'
 
 import LevelModal from 'components/user/LevelModal'
 import Modal from 'components/common/Modal'
+import toast from 'components/common/Toast'
 
 import { BiEdit } from 'react-icons/bi'
+import { HiLightBulb } from 'react-icons/hi'
 import { UserRecord, User } from 'types/user.interface'
 import LEVEL_TO_IMG from 'constants/levels'
 import bell from 'assets/images/bell.png'
 
-import { logout } from 'apis/services/users'
+import { logout, report } from 'apis/services/users'
 import useUserQuery from 'hooks/useUserQuery'
 
 interface UserProfileProps extends UserRecord, User {}
 
-// Todo: level API 업데이트
 export default function UserProfile({
   profileUrl,
   nickname,
@@ -36,15 +37,23 @@ export default function UserProfile({
   const { theme } = useContext(ThemeContext)
   const navigate = useNavigate()
   const [isOpen, setIsOpen] = useState(false)
-  const { nickname: userNickname } = useParams()
+  const { nickname: userNickname } = useParams() as { nickname: string }
   const { data: userInfo } = useUserQuery()
   const onClickLogout = () => {
     logout()
     navigate('/')
   }
 
-  // 내 자신이 아니면 로그아웃, 수정, 알람버튼 숨기기
-  const stranger = userNickname === userInfo?.nickname ? '' : styles.stranger
+  const onClickReport = () => {
+    report('MEMBER', userNickname).then(() => {
+      toast.addMessage('success', '신고가 완료됐습니다')
+    })
+  }
+
+  // 내 자신이 아니면
+  // 로그아웃, 수정, 알람버튼 숨기기
+  // 신고하기 보이기
+  const stranger = userNickname !== userInfo?.nickname ? styles.stranger : ''
 
   return (
     <div className={styles.profile}>
@@ -53,8 +62,13 @@ export default function UserProfile({
           <LevelModal />
         </Modal>
       )}
-      <div className={`${styles.alarm} ${stranger} `}>
-        <IconButton imgSrc={bell} onClick={() => navigate('/alarm')} />
+      <div className={`${styles['alarm-siren']}  ${stranger}`}>
+        <div className={`${styles.siren}`} onClick={onClickReport}>
+          <HiLightBulb /> 신고하기
+        </div>
+        <div className={`${styles.alarm}`}>
+          <IconButton imgSrc={bell} onClick={() => navigate('/alarm')} />
+        </div>
       </div>
       <div className={`content ${theme} ${styles.img}`}>
         <Image size="lg" imgUrl={profileUrl} />
