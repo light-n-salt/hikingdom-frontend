4fc36a6106188f9eeeb77ee71c1ede9e62e0dd81
teamzamong <teamzamong@gmail.com>
2023-05-12 16:41:25 +0900

    BE:fix: fix security config for websocket

--- backend/service/src/main/java/org/lightnsalt/hikingdom/service/config/WebSocketConfig.java
+++ backend/service/src/main/java/org/lightnsalt/hikingdom/service/config/WebSocketConfig.java
@@ -1,20 +1,14 @@
-package org.lightnsalt.hikingdom.chat.config;
+package org.lightnsalt.hikingdom.service.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import lombok.RequiredArgsConstructor;
-
 @Configuration
-@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-	private final StompHandler stompHandler;
-
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
 		config.enableSimpleBroker("/sub");
@@ -23,16 +17,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/chat/ws")
+		registry.addEndpoint("/api/v1/hiking/ws")
 			.setAllowedOriginPatterns("http://localhost:3000", "http://localhost:8080", "https://hikingdom.kr",
 				"http://hikingdom.kr:3001", "http://hikingdom.kr:8081", "http://hikingdom.kr:8080",
-				"https://k8a102.p.ssafy.io")
+				"https://k8a102.p.ssafy.io", "http://70.12.246.181:3000")
 			.withSockJS();
-	}
-
-	@Override
-	public void configureClientInboundChannel(ChannelRegistration registration) {
-		registration.interceptors(stompHandler);
 	}
 }
 
