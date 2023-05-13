e18225374aecfa7169a37ac706b123b7a0e8839b
heeheejj <kjyk0835@naver.com>
2023-05-14 02:47:41 +0900

    BE:feat: add Notification Entity

---
+++ backend/domain/src/main/java/org/lightnsalt/hikingdom/domain/entity/notification/Notification.java
@@ -0,0 +1,48 @@
+package org.lightnsalt.hikingdom.domain.entity.notification;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.lightnsalt.hikingdom.domain.common.BaseTimeEntity;
+import org.lightnsalt.hikingdom.domain.entity.member.Member;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "member_hiking")
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BIGINT UNSIGNED")
+    private Long id;
+
+    @ToString.Exclude
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Member member;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false, length = 100)
+    private String body;
+
+    @Column(name = "send_at")
+    private LocalDateTime sendAt;
+
+    @Column(name = "is_read", nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
+    private boolean isRead;
+
+    @Builder
+    public Notification(Member member, String title, String body, LocalDateTime sendAt, boolean isRead) {
+        this.member = member;
+        this.title = title;
+        this.body = body;
+        this.sendAt = sendAt;
+        this.isRead = isRead;
+    }
+}
