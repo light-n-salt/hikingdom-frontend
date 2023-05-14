e7ac011fd3ef3aaa52c1ee14ad5869ada05f17aa
teamzamong <teamzamong@gmail.com>
2023-05-14 20:43:50 +0900

    BE:refactor: small fixes to improve error messages

--- backend/service/src/main/java/org/lightnsalt/hikingdom/service/member/dto/request/MemberEmailAuthenticationReq.java
+++ backend/service/src/main/java/org/lightnsalt/hikingdom/service/member/dto/request/MemberEmailAuthenticationReq.java
@@ -1,5 +1,6 @@
 package org.lightnsalt.hikingdom.service.member.dto.request;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
@@ -11,7 +12,7 @@ public class MemberEmailAuthenticationReq {
 	@Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
 	private String email;
 
-	@NotEmpty(message = "인증번호는 필수 입력값입니다.")
-	@Pattern(regexp = "^([0-9]{6})$", message = "인증번호 형식에 맞지 않습니다.")
+	@NotBlank(message = "인증코드를 입력해주세요.")
+	@Pattern(regexp = "^([0-9]{6})?$", message = "올바른 인증코드를 입력해주세요.")
 	private String authCode;
 }
