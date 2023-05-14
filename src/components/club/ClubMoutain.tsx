2dc77397ad4755cedb0705d441f8994aa4d91681
hyejin0705 <hyejin3681@gmail.com>
2023-05-15 02:53:12 +0900

    FE:feat: Create multiple land assets

--- src/components/club/ClubMoutain.tsx
+++ src/components/club/ClubMoutain.tsx
@@ -8,13 +8,14 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 // Mesh의 postion 타입 정의 (x축, y축, z축)
 interface CubeProps {
   position?: THREE.Vector3
+  url: string
 }
 
 // Mesh 생성 함수
-function Cube({ position, ...props }: CubeProps) {
+function Cube({ position, url, ...props }: CubeProps) {
   const gltf = useLoader(
     GLTFLoader,
-    'https://lightnsalt.s3.ap-northeast-2.amazonaws.com/asset/main5.gltf'
+    url,
   )
 
   // const ref = useRef<THREE.Mesh>(null)
@@ -30,9 +31,10 @@ function Cube({ position, ...props }: CubeProps) {
 
   return (
     <primitive
-      // {...props}
-      // ref={ref}
-      object={gltf.scene}
+      // React Three Fiber에서는 각 요소가 고유한 ID를 가져야 중복 가능
+      // scene 속성을 가져와 <primitive> 요소의 object 속성에 할당 => 자체 고유한 ID 가능
+      // gltf.scene.clone()을 사용하여, 복사된 THREE.Group을 할당
+      object={gltf.scene.clone()}  // 고유한 ID 생성
       position={position}
       scale={clicked ? 1.5 : 1}
       onClick={() => click(!clicked)}
@@ -62,12 +64,17 @@ function ClubMountain() {
       <pointLight position={[10, 10, 10]} />
       {/* 일광과 같이, 아주 먼 거리의 광원에서 평행으로 진행하는 빛 */}
 
-      <Cube position={new THREE.Vector3(0, 0, -5)} />
-      {/* <Cube position={new Vector3(0, 0, -30)} /> */}
-
+      <Cube position={new THREE.Vector3(-1, 0, 0)} url='https://lightnsalt.s3.ap-northeast-2.amazonaws.com/asset/main5.gltf'/>
+      {/* <Cube position={new THREE.Vector3(2, 0, 0)} url='https://lightnsalt.s3.ap-northeast-2.amazonaws.com/asset/main5.gltf'/> */}
+      {/* <Cube position={new THREE.Vector3(2, 0, 3)} url='https://lightnsalt.s3.ap-northeast-2.amazonaws.com/asset/main5.gltf'/> */}
+      
+      <Cube position={new THREE.Vector3(-1.89, 0.3, 2.1)} url='https://lightnsalt.s3.ap-northeast-2.amazonaws.com/asset/land3.gltf'/>
+      <Cube position={new THREE.Vector3(-0.77, 0.3, 2.1)} url='https://lightnsalt.s3.ap-northeast-2.amazonaws.com/asset/land3.gltf'/>
+      <Cube position={new THREE.Vector3(0.36, 0.3, 2.1)} url='https://lightnsalt.s3.ap-northeast-2.amazonaws.com/asset/land3.gltf'/>
+      
       {/* target: 카메라의 주시점. x, y, z 축 순으로 설정할 수 있다.
           enableDamping: true를 설정할 경우, 드래그 시의 애니메이션을 부드럽게 한다. 디폴트 값은 false. */}
-      <OrbitControls target={[0, 0, 0]} enableDamping={true} />
+      <OrbitControls enableDamping={true} />
     </Canvas>
   )
 }
