254095964479148be9c2f801ca6680f9fcf838b2
이현진 <dnzma13@naver.com>
2023-05-16 00:25:06 +0900

    AND:feat: add popup hiking type dialog functionality in HikingFragment - popup dialog_select_meetup.xml when Meetup Hiking is clicked - popup dialog_select_mountain.xml when Individual Hiking is clicked

--- android/hikingdom/app/src/main/java/com/example/hikingdom/ui/main/hiking/dialog/MeetupAdapter.kt
+++ android/hikingdom/app/src/main/java/com/example/hikingdom/ui/main/hiking/dialog/MeetupAdapter.kt
@@ -1,34 +1,39 @@
 package com.example.hikingdom.ui.main.hiking.dialog
 
 import android.content.Context
-import android.graphics.Color
-import android.graphics.PorterDuff
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.hikingdom.R
 import com.example.hikingdom.data.remote.hiking.Meetup
-import com.example.hikingdom.data.remote.hiking.Mountain
-import com.google.gson.annotations.SerializedName
 
-class MeetupAdapter(val context: Context, val dataset:List<Meetup>): RecyclerView.Adapter<MeetupAdapter.ViewHolder>(){
+class MeetupAdapter(val context: Context, val dataset: List<Meetup>) :
+    RecyclerView.Adapter<MeetupAdapter.ViewHolder>() {
 
-    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val meetupName = view.findViewById<TextView>(R.id.meetup_name)
         val mountainName = view.findViewById<TextView>(R.id.mountain_name)
         val totalMember = view.findViewById<TextView>(R.id.total_member)
         val startAt = view.findViewById<TextView>(R.id.start_at)
 
-        fun bind(meetup: Meetup){
+        fun bind(meetup: Meetup) {
             meetupName.text = meetup.meetupName
             mountainName.text = meetup.mountainName
             totalMember.text = meetup.totalMember.toString()
             startAt.text = meetup.startAt
+        }
+
+        init {
+//            view.setOnClickListener {
+//                view.setBackgroundColor(
+//                    ContextCompat.getColor(
+//                        context,
+//                        R.color.secondary
+//                    )
+//                )
+//            }
         }
     }
 
