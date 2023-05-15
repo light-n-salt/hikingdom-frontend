8a61500fdf9493e73389f272c913310375e36466
teamzamong <teamzamong@gmail.com>
2023-05-15 13:14:39 +0900

    BE:fix: update login API to save fcm token if exists

--- backend/service/src/main/java/org/lightnsalt/hikingdom/service/member/repository/MemberFcmTokenRepository.java
+++ backend/service/src/main/java/org/lightnsalt/hikingdom/service/member/repository/MemberFcmTokenRepository.java
@@ -1,4 +1,8 @@
 package org.lightnsalt.hikingdom.service.member.repository;
 
-public interface MemberFcmTokenRepository {
+import org.lightnsalt.hikingdom.domain.entity.notification.MemberFcmToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberFcmTokenRepository extends JpaRepository<MemberFcmToken, Long> {
+	boolean existsByMemberIdAndBody(Long memberId, String body);
 }
