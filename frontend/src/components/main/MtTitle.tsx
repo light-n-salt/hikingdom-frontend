baad35a9cf24cf195294cd23bc12fd76c1c180ec
Yeji <yzii9841@gmail.com>
2023-05-15 18:02:45 +0900

    FE:feat: add animation to assets

--- frontend/src/components/main/MtTitle.tsx
+++ frontend/src/components/main/MtTitle.tsx
@@ -1,7 +1,15 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { ThemeContext } from 'styles/ThemeProvider'
 import styles from './MtTitle.module.scss'
 import { convertToHour } from 'utils/convertToTime'
+
+import Loading from 'components/common/Loading'
+import Modal from 'components/common/Modal'
+import AssetModal from './AssetModal'
+
+import * as THREE from 'three'
+import { Canvas } from '@react-three/fiber'
+import Asset from './Asset'
 
 type MtTitleProps = {
   name: string
@@ -12,8 +20,31 @@ type MtTitleProps = {
 
 function MtTitle({ name, maxAlt, timeDuration, assetUrl }: MtTitleProps) {
   const { theme } = useContext(ThemeContext)
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className={`content ${theme} ${styles.mttitle}`}>
+      {/* 모달 */}
+      {isOpen && (
+        <Modal onClick={() => setIsOpen(false)}>
+          <AssetModal />
+        </Modal>
+      )}
+      {/* asset */}
+      <div className={styles.asset}>
+        {assetUrl ? (
+          <Canvas>
+            <ambientLight />
+            <Asset
+              position={new THREE.Vector3(0, -0.5, 3)}
+              url={assetUrl}
+              onClick={() => setIsOpen(true)}
+            />
+          </Canvas>
+        ) : (
+          <Loading />
+        )}
+      </div>
       <div className={styles.title}>{name}</div>
       <div className={styles.container}>
         {/* 높이 */}
@@ -29,13 +60,7 @@ function MtTitle({ name, maxAlt, timeDuration, assetUrl }: MtTitleProps) {
           <span className={styles.bold}>약 {convertToHour(timeDuration)}</span>
           <span className={styles.text}>왕복시간</span>
         </div>
-
         <span className={styles.border}>|</span>
-
-        {/* asset */}
-        <div className={styles.content}>
-          <img src={assetUrl} className={styles.img} />
-        </div>
       </div>
     </div>
   )
