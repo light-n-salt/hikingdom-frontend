3361199c484058c317200ab902fd5925b49543ad
Yeji Jung <yzii9841@gmail.com>
2023-05-16 00:13:40 +0900

    FE:feat: zoom asset when clicked

--- frontend/src/components/main/Asset.tsx
+++ frontend/src/components/main/Asset.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState } from 'react'
 import styles from './Asset.module.scss'
 import * as THREE from 'three'
 import { useLoader, useFrame } from '@react-three/fiber'
@@ -13,14 +13,31 @@ interface AssetProps {
 
 function Asset({ onClick, position, url }: AssetProps) {
   const asset = useLoader(GLTFLoader, url)
+  const [click, setClick] = useState(false)
+  const scaleRef = useRef(1)
 
   // 중심 기준으로 회전
   useFrame((state, delta) => {
     asset.scene.rotation.y += delta
   })
 
+  const onClickZoom = () => {
+    onClick()
+    setClick(true)
+  }
+
+  // 클릭시 크기 커짐
+  // useFrame(() => {
+  //   if (click && scaleRef.current < 1) {
+  //     scaleRef.current += 0.01
+  //   } else if (!click && scaleRef.current > 1) {
+  //     scaleRef.current -= 0.01
+  //   }
+  //   asset.scene.scale.set(scaleRef.current, scaleRef.current, scaleRef.current)
+  // })
+
   return (
-    <primitive object={asset.scene} position={position} onClick={onClick} />
+    <primitive object={asset.scene} position={position} onClick={onClickZoom} />
   )
 }
 
--- frontend/src/components/main/AssetModal.tsx
+++ frontend/src/components/main/AssetModal.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
-function AssetModal() {
-  return <div>AssetModal</div>
+type AssetModalProps = {
+  url: string
+  name: string
+  getCondition: string
+}
+
+function AssetModal({ url, name, getCondition }: AssetModalProps) {
+  return (
+    <div>
+      <div> {name}</div>
+      <div>획득 조건 : {getCondition}</div>
+    </div>
+  )
 }
 
 export default AssetModal
