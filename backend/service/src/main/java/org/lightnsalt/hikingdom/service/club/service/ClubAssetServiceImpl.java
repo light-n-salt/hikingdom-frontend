44046f08a3c228d3e545e4fc39675944340b7ddc
teamzamong <teamzamong@gmail.com>
2023-05-17 14:33:42 +0900

    BE:fix: add default asset to response for club detail

--- backend/service/src/main/java/org/lightnsalt/hikingdom/service/club/service/ClubAssetServiceImpl.java
+++ backend/service/src/main/java/org/lightnsalt/hikingdom/service/club/service/ClubAssetServiceImpl.java
@@ -3,13 +3,17 @@ package org.lightnsalt.hikingdom.service.club.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
+
 import org.lightnsalt.hikingdom.common.error.ErrorCode;
 import org.lightnsalt.hikingdom.common.error.GlobalException;
 import org.lightnsalt.hikingdom.domain.entity.club.Club;
 import org.lightnsalt.hikingdom.domain.entity.club.ClubAsset;
+import org.lightnsalt.hikingdom.domain.entity.info.AssetInfo;
+import org.lightnsalt.hikingdom.service.club.dto.response.ClubAssetRes;
 import org.lightnsalt.hikingdom.service.club.repository.ClubAssetRepository;
 import org.lightnsalt.hikingdom.service.club.repository.ClubRepository;
-import org.lightnsalt.hikingdom.service.club.dto.response.ClubAssetRes;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -21,12 +25,24 @@ import lombok.extern.slf4j.Slf4j;
 public class ClubAssetServiceImpl implements ClubAssetService {
 	private final ClubRepository clubRepository;
 	private final ClubAssetRepository clubAssetRepository;
+	@Value("${values.asset.defaultAsset}")
+	private String defaultAssetUrl;
+	private AssetInfo defaultAsset;
+
+	@PostConstruct
+	private void postConstruct() {
+		defaultAsset = new AssetInfo(null, "기본 에셋", defaultAssetUrl, "기본 에셋", 0);
+	}
 
 	@Override
 	public List<ClubAssetRes> findClubMountainList(Long clubId) {
 		final Club club = clubRepository.findById(clubId)
 			.orElseThrow(() -> new GlobalException(ErrorCode.CLUB_NOT_FOUND));
-		final List<ClubAsset> clubAssetList = clubAssetRepository.findAllByClubId(club.getId());
+		List<ClubAsset> clubAssetList = clubAssetRepository.findAllByClubId(club.getId());
+
+		// 기본 땅 에셋 추가하기
+		ClubAsset defaultClubAsset = new ClubAsset(club, defaultAsset, null, 0, 0);
+		clubAssetList.add(0, defaultClubAsset);
 
 		return clubAssetList.stream()
 			.map(ClubAssetRes::new).collect(Collectors.toList());
