015b0b90c81ea62f9e93f61d4283236c97f3d5d8
hyejin0705 <hyejin3681@gmail.com>
2023-05-17 09:25:01 +0900

    FE:style:update Dropdown CSS in SearchClubMt

--- src/components/club/SearchClubMt.tsx
+++ src/components/club/SearchClubMt.tsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react'
 import styles from './SearchClubMt.module.scss'
 import { AssetInfo } from 'types/club.interface'
 // import SearchBar from 'components/common/SearchBar'
-// import Dropdown from 'components/common/Dropdown'
+import Dropdown from 'components/common/Dropdown'
 import ClubMountain from 'components/club/ClubMountain'
 import SelectBox from 'components/common/SelectBox'
 import { getPosition } from 'utils/getPosition'
 
 type SearchClubMtProps = {
-  assetInfo: AssetInfo[];
-};
+  assetInfo: AssetInfo[]
+}
 
 function SearchClubMt({ assetInfo }: SearchClubMtProps) {
   const [filterOptions, setFilterOptions] = useState<any[]>([])
@@ -18,14 +18,17 @@ function SearchClubMt({ assetInfo }: SearchClubMtProps) {
 
   const [filter, setFilter] = useState('') // 선택된 필터 옵션 value
 
-  useEffect (() => {
+  useEffect(() => {
     const data = getPosition(assetInfo, filter)
     setAssetArray(data.arr)
 
-    const options = data.uniqueNameList.map((name) => ({ label: name, value: name }));
+    const options = data.uniqueNameList.map((name) => ({
+      label: name,
+      value: name,
+    }))
     setFilterOptions(options)
     console.log('check')
-  }, [filter]) 
+  }, [filter])
 
   // function onChangeSetValue(event: React.ChangeEvent<HTMLInputElement>) {
   //   setValue(event.target.value)
@@ -33,15 +36,19 @@ function SearchClubMt({ assetInfo }: SearchClubMtProps) {
 
   return (
     <>
+      <ClubMountain zoom={3.5} assetInfo={assetArray} />
       <div className={styles.select}>
-        <SelectBox options={filterOptions} setValue={setFilter} defaultLabel='전체'/>
+        <Dropdown
+          options={filterOptions}
+          setValue={setFilter}
+          defaultLabel="전체"
+        />
       </div>
       {/* <SearchBar
         value={value}
         placeholder="등산했던 산을 검색해보세요"
         onChange={onChangeSetValue}
       /> */}
-      <ClubMountain zoom={3.5} assetInfo={assetArray} />
     </>
   )
 }
