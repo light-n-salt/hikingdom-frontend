5db09cee4d504795247925487d2230e6463245ca
Yeji <yzii9841@gmail.com>
2023-05-17 10:01:31 +0900

    FE:style: chagnge logo.png file and IndexPage CSS

--- frontend/src/pages/auth/IndexPage.tsx
+++ frontend/src/pages/auth/IndexPage.tsx
@@ -9,12 +9,16 @@ function IndexPage() {
   const navigate = useNavigate()
 
   return (
-    <>
+    <div className={`page p-lg ${styles.container}`}>
       <video className={styles.video} autoPlay loop muted>
         <source src={mountainVideo} />
       </video>
-      <div className={`page p-lg ${styles.container}`}>
-        <Logo size="lg" />
+
+      <div className={styles.content}>
+        <div className={styles.slogan}>
+          <Logo size="lg" />
+          <div>등산으로 완성하는 우리의 왕국</div>
+        </div>
         <Button
           text="시작하기"
           color="primary"
@@ -22,7 +26,7 @@ function IndexPage() {
           onClick={() => navigate('/login')}
         />
       </div>
-    </>
+    </div>
   )
 }
 
