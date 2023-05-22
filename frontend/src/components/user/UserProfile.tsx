79d8dd7f58ab2fdfda37d7126a1d917086935232
hyejin0705 <hyejin3681@gmail.com>
2023-05-22 18:02:05 +0900

    FE:feat:update userProfile CSS

--- frontend/src/components/user/UserProfile.tsx
+++ frontend/src/components/user/UserProfile.tsx
@@ -104,7 +104,7 @@ export default function UserProfile({
             />
           )}
         </div>
-        <span className={styles.email}>{email}</span>
+        <span className={`${stranger} ${styles.email}`}>{email}</span>
         <UserInfo
           totalAlt={totalAlt}
           totalDistance={totalDistance}
