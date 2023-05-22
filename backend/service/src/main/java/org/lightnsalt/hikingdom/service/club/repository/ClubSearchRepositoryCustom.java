ecca3c3a7b6ad4f56c94278010c37a4e2d2e8af9
teamzamong <teamzamong@gmail.com>
2023-05-22 14:30:34 +0900

    BE:fix: update findClubs method to sort null rankings to last

--- backend/service/src/main/java/org/lightnsalt/hikingdom/service/club/repository/ClubSearchRepositoryCustom.java
+++ backend/service/src/main/java/org/lightnsalt/hikingdom/service/club/repository/ClubSearchRepositoryCustom.java
@@ -35,7 +35,7 @@ public class ClubSearchRepositoryCustom extends ClubSearchRepository {
 			.leftJoin(clubRanking)
 			.on(club.id.eq(clubRanking.club.id).and(clubRanking.setDate.eq(today)))
 			.where(whereClause)
-			.orderBy(clubRanking.ranking.asc())
+			.orderBy(clubRanking.ranking.asc().nullsLast())
 			.limit(pageable.getPageSize() + 1L)
 			.fetch();
 
